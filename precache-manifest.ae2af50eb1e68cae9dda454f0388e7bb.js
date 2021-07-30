75ce7bc5e2f58f664d7aae2d836297e9a0433ede
Goblester <goblin829@gmail.com>
2021-07-31 02:47:09 +0300

    Updates

---
+++ precache-manifest.ae2af50eb1e68cae9dda454f0388e7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62aa8ba3ff24d3f1b182eab5fb55a59",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "8b7d8baf793d3574ebf3",
+    "url": "/todo-list/static/css/main.674b6137.chunk.css"
+  },
+  {
+    "revision": "c54787b64f522eebf76b",
+    "url": "/todo-list/static/js/2.985f8dbb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo-list/static/js/2.985f8dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7d8baf793d3574ebf3",
+    "url": "/todo-list/static/js/main.f08307d5.chunk.js"
+  },
+  {
+    "revision": "f5ee74d33c5afdf3bdbc",
+    "url": "/todo-list/static/js/runtime-main.bd857350.js"
+  }
+]);
